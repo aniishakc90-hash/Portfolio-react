7e72788ff14d2c4ec5efb45531f52213a3f88ef2
aniisha <aniishakc90@gmail.com>
2026-02-25 08:22:30 +0545

    first commit

---
+++ src/contact/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header"
+
+export const Contact = () => {
+  return (
+    <div>
+      <Header />
+        <h1>Contact</h1>
+        <div>Contact Page</div>
+    </div>
+  
+  );
+}
